4d229098c49651ca0e4b03c82bf80e0ab2562b71
hsluoyz <hsluoyz@qq.com>
2015-12-02 21:21:43 +0800

    Now TeamHelper can get the meeting info by database.

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 # coding=gbk
 
 import MySQLdb
+import re
 # import sys
 # reload(sys)
 # sys.setdefaultencoding('utf-8')
@@ -13,25 +14,33 @@ except:
     db_credential = {'host': 'something.com', 'user': 'xxx', 'password': '123', 'database': 'xxx'}
 
 
+def getMeetingInfo():
+    conn = MySQLdb.connect(
+        host = db_credential['host'],
+        user = db_credential['user'],
+        passwd = db_credential['password'],
+        db = db_credential['database'],
+        charset = "utf8"
+    )
 
-conn = MySQLdb.connect(
-    host = db_credential['host'],
-    user = db_credential['user'],
-    passwd = db_credential['password'],
-    db = db_credential['database'],
-    charset = "utf8"
-)
+    cursor = conn.cursor()
+    sql = "SELECT * FROM store WHERE `key`='pad:meeting'"
 
-cursor = conn.cursor()
-sql = 'SELECT * FROM store'
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            p = re.compile('"text":"(.*)","attribs"')
+            m = p.search(row[1])
+            if m:
+                str = m.group(1).replace('\\n', '\n')
+                print 'Meeting info from website:\n' + str
+                return str
+        return ''
+    except Exception, e:
+        print e
+        return ''
 
-try:
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    for row in results:
-        print "key = %s, value = %s" % (row[0], row[1])
+    conn.close()
 
-except Exception, e:
-    print e
-
-conn.close()
+getMeetingInfo()
