ff8daa5ff5442916e00e9cb5656f52ce17d6ec21
hsluoyz <hsluoyz@qq.com>
2015-12-02 15:29:23 +0800

    Added 'Cc' function in Email sending.

--- mail.py
+++ mail.py
@@ -6,14 +6,16 @@ from email.mime.text import MIMEText
 try:
     import user_data
     mail_credential = user_data.mail_credential
+    receive_list = user_data.receive_list
+    copy_list = user_data.copy_list
 except:
     print 'No user data, use default value'
     mail_credential = {'address': 'xxx@something.com', 'password': '123'}
+    receive_list=['receive@something.com']
+    copy_list=['cc@qq.something.com']
 
 
-receive_list=['admin@osvt.net', 'hsluoyz@qq.com']
-
-def send_mail(cred, receive_list, sub, content):
+def send_mail(cred, receive_list, copy_list, sub, content):
     mail_address = cred['address']
     mail_pass = cred['password']
     mail_host = mail_address.split('@')[1]
@@ -23,16 +25,17 @@ def send_mail(cred, receive_list, sub, content):
     msg['Subject'] = sub
     msg['From'] = me
     msg['To'] = ";".join(receive_list)
+    msg['Cc'] = ";".join(copy_list)
     try:
         server = smtplib.SMTP()
         server.connect(mail_host)
         server.login(mail_address, mail_pass)
-        server.sendmail(me, receive_list, msg.as_string())
+        server.sendmail(me, receive_list + copy_list, msg.as_string())
         server.quit()
         return True
     except Exception, e:
         print str(e)
         return False
 
-send_mail(mail_credential, receive_list, "Title", "hello world£¡")
+send_mail(mail_credential, receive_list, copy_list, "Title", "hello world£¡")
 
