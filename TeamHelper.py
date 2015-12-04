c6f48f3a3c58e0bdfb05c0b5bd1f43bddc44cc56
hsluoyz <hsluoyz@qq.com>
2015-12-04 15:48:39 +0800

    Added scheduling function by using APScheduler.

--- TeamHelper.py
+++ TeamHelper.py
@@ -1,57 +1,16 @@
 # coding=gbk
 
-import qq
-import database
-
-try:
-    import user_data
-    user_list = user_data.user_list
-except:
-    print 'No user data, use default value'
-    user_list = {}
-    user_list['ly'] = {'name': 'ly', 'email': 'xxx@something.com', 'spell': 'osvtzhuli'}
+import task
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def analyzeProgress(str):
-    res = ''
-    if str.find(u'没有按时写，下次注意') != -1:
-        res = u'组会预告未填写！'
-    elif len(str) < 8:
-        res = u'组会预告长度过短，至少8个字！'
-    else:
-        res = ''
-    # print "Text: %s, Result: %s" %(str, res)
-    return res
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(task.task_RetrieveWebsiteAndSendReport, 'cron', day_of_week='fri', hour=15, minute=46)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
-def printRemarks():
-    for name in user_list.keys():
-        info = user_list[name]
-        print "%s ||| %s ||| %s" % (info['cname'], info['progress'], info['remark'])
-
-
-def performEvaluation():
-    name_progress = database.getMeetingInfo()
-    # print '\n'.join(name_progress)
-
-    print ''
-    for cname in name_progress.keys():
-        progress = name_progress[cname]
-        for name in user_list.keys():
-            info = user_list[name]
-            if info['cname'] == cname:
-                info['progress'] = progress
-                info['remark'] = analyzeProgress(progress)
-    printRemarks()
-
-def generateQQReport():
-    text = u'大家好，目前组会预告还有些问题，请大家完善后提交到http://osvt.net:9000/p/meeting：\n'
-    for name in user_list.keys():
-        info = user_list[name]
-        if info['remark'] != '':
-            text = text + u'@' + info['name'] + u'：' + info['remark'] + '\n'
-    print 'Report:\n' + text
-    return text
-
-performEvaluation()
-report = generateQQReport()
-qq.QQ_SendTextWithAt(report)
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
