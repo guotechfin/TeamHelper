c4ac1fccfff89d92ac1711766e03a30547a700a6
hsluoyz <hsluoyz@qq.com>
2015-12-04 17:47:30 +0800

    Rescheduled the time for those events.

--- TeamHelper.py
+++ TeamHelper.py
@@ -2,12 +2,20 @@
 
 import task
 import os
+import time
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 if __name__ == '__main__':
+    ISOTIMEFORMAT = '%Y-%m-%d %X'
+    print time.strftime(ISOTIMEFORMAT, time.localtime())
+
     scheduler = BlockingScheduler()
-    scheduler.add_job(task.task_RetrieveWebsiteAndSendQQReport, 'cron', day_of_week='fri', hour=15, minute=46)
+    scheduler.add_job(task.task_SendQQMeetingNotice,            'cron', day_of_week='fri', hour=20, minute=00)
+    scheduler.add_job(task.task_RetrieveWebsiteAndSendQQReport, 'cron', day_of_week='sun', hour=12, minute=00)
+    scheduler.add_job(task.task_RetrieveWebsiteAndSendQQReport, 'cron', day_of_week='sun', hour=20, minute=00)
+    scheduler.add_job(task.task_RetrieveWebsiteAndSendQQReport, 'cron', day_of_week='sun', hour=21, minute=55)
+    scheduler.add_job(task.task_SendNextMeetingMails,           'cron', day_of_week='sun', hour=22, minute=00)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
