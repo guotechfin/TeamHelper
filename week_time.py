d037af2e56cbf140afe0dcb754834c73548e3ccc
hsluoyz <hsluoyz@qq.com>
2016-09-04 12:30:17 +0800

    Improved the minus week number handling.

--- week_time.py
+++ week_time.py
@@ -1,13 +1,13 @@
 # coding=gbk
 
 from datetime import datetime
-
+import math
 
 def getWeekNo():
-    date1 = datetime.strptime('2016-2-22','%Y-%m-%d')
+    date1 = datetime.strptime('2016-9-12','%Y-%m-%d')
     date2 = datetime.now()
-    days = int((date2 - date1).total_seconds() / 24 / 3600)
-    return days / 7 + 1
+    days = (date2 - date1).total_seconds() / 24 / 3600
+    return int(math.floor(days / 7)) + 1
 
 def getWeekDay():
     date1 = datetime.strptime('2016-2-22','%Y-%m-%d')
